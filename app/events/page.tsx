8ce0d44dc53df218bf78249834cd59d48bf5a153
palatebyblackoak <info@blackoakagency.co>
2026-07-31 11:21:30 -0500

    Events hero: swap stock wine-glasses shot for the patio firepit

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -23,7 +23,7 @@ export default function EventsPage() {
         label="Events"
         headline="Events at Republic."
         subhead="Private dining, special programming, and celebrations worth remembering."
-        image={img.eventsHero}
+        image={img.patioNight}
         overlay={0.65}
       />
 
